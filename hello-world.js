8559556795653864d1ef883f2bbe7f073246c2ef
tobisamcode <samueloluwatobiloba48@gmail.com>
2022-04-01 13:48:08 +0100

    node

--- hello-world.js
+++ hello-world.js
@@ -1,17 +1,14 @@
 const http = require('http');
 
-
+const port = 3001;
 const hostname = '127.0.0.1';
-const port = 3000;
-
-
 
 const server = http.createServer((req, res) => {
     res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('Hello, World!\n');
-});
+    res.setHeader = ('Content-Type', 'text/plain');
+    res.end('hello-world');
+})
 
 server.listen(port, hostname, () => {
     console.log(`server running at http://${hostname}:${port}/`)
-});
+})
