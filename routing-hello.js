b756d3c5f7042bd028af34fc8342a9d55357df39
tobisamcode <samueloluwatobiloba48@gmail.com>
2022-04-03 18:40:25 +0100

    day 1

--- routing-hello.js
+++ routing-hello.js
@@ -1,4 +1,20 @@
 var http = require('http');
+fs = require('fs');
+
+function serveStaticFile(res, path, contentType, responseCode) {
+    if (!responseCode) responseCode = 200;
+    fs.readFile(__dirname + path, function(err, data) {
+        if (err) {
+            res.writeHead(500, { 'Content-Type': 'text/plain' });
+            res.end('500 - Internal Error')
+        } else {
+            res.writeHead(responseCode, { 'Content-Type': contentType });
+            res.end(data);
+        }
+    });
+}
+
+
 
 http.createServer(function(req, res) {
     // normalize url by removing querystring, optional
@@ -8,16 +24,17 @@ http.createServer(function(req, res) {
 
     switch (path) {
         case '':
-            res.writeHead(200, { 'Content-Type': 'text/plain' });
-            res.end('Homepage');
+            serveStaticFile(res, '/public/home.html', 'text/html');
             break;
         case '/about':
-            res.writeHead(200, { 'Content-Type': 'text/plain' });
-            res.end('About');
+            serveStaticFile(res, '/public/about.html', 'text/html');
             break;
+        case '/img/logo.jpg':
+            serveStaticFile(res, '/public/img/logo.jpg', 'image/jpeg');
+            break
         default:
-            res.writeHead(404, { 'Content-Type': 'text/plain' });
-            res.end('Not Found');
+            serveStaticFile(res, '/public/404.html', 'text/html',
+                404);
             break;
     }
 }).listen(8080);
