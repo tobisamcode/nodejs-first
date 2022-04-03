1dcb96fd08e3a4c52e73d68c834e4f109c7b7b1d
tobisamcode <samueloluwatobiloba48@gmail.com>
2022-04-03 22:17:27 +0100

    using the pipe and stream to read data

--- hello.js
+++ hello.js
@@ -1,8 +1,10 @@
 const http = require('http');
+var fs = require('fs');
 
 http.createServer((req, res) => {
     res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Hello');
+    var myReadStream = fs.createReadStream(__dirname + '/readMe.txt', 'utf-8');
+    myReadStream.pipe(res);
 }).listen(3000);
 
 console.log('started localhost on 3000 ')
